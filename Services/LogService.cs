566e7cfe97befc6cc8b74094aa712cfd19839774
william dew <williamrdew@gmail.com>
2026-04-12 15:55:12 +0100

    refined LogService.cs

---
+++ Services/LogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace AssetGuard.Services
+{
+    public class LogService(string logFilePath)
+    {
+        public void LogAction(string action)
+        {
+            var logEntry = $"{DateTime.Now:dd-MM-yyyy HH:mm:ss} - {action}{Environment.NewLine}";
+            File.AppendAllText(logFilePath, logEntry);
+        }
+
+        public string LogFilePath => logFilePath;
+    }
+}
